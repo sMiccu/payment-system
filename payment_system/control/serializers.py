2ac1f8de0ea504aff4b0cb80004c38feeeae3e50
TAISEI NISHI <likliklik0518@gmail.com>
2025-11-20 09:54:24 +0000

    add : Storeシリアライザーのpricing_logic

--- payment_system/control/serializers.py
+++ payment_system/control/serializers.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 class StoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Store
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'pricing_logic']
 
 class DailyStoreSalesSerializer(serializers.ModelSerializer):
     class Meta:
